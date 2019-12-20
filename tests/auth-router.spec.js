01d0db319d190a5be14f91bd11e13d83ee305bcb
Maddy <mmcintosh2k14@gmail.com>
2019-12-20 13:14:12 -0500

    creates first test for POST register endpoint

--- tests/auth-router.spec.js
+++ tests/auth-router.spec.js
@@ -4,10 +4,10 @@ const request = require('supertest');
 const server = require('../api/server');
 
 describe('tests server.js', function() {
-	describe('POST /register', function() {
+	describe('POST /api/auth/register', function() {
 		it('should register a new user and return 201 OK', function() {
 			return request(server)
-				.post('/register')
+				.post('/api/auth/register')
 				.send({ username: 'Benny', password: 'broham' })
 				.then(res => {
 					expect(res.status).toBe(201);
