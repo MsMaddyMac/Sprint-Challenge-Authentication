a36b2b59bcc0d6ca399167180a63b1ef3283cd18
Maddy <mmcintosh2k14@gmail.com>
2019-12-20 11:51:36 -0500

    creates POST endpoint for login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -21,8 +21,33 @@ router.post('/register', (req, res) => {
 		});
 });
 
+// POST endpoint to log user in
 router.post('/login', (req, res) => {
 	// implement login
+	let { username, password } = req.body;
+
+	Users.findBy({ username })
+		.first()
+		.then(user => {
+			if (user && bcrypt.compareSync(password, user.password)) {
+				// sign token
+				const token = signToken(user);
+
+				// send token
+				res.status(200).json({
+					token,
+					message: `Well, howdy do ${user.username}!`
+				});
+			} else {
+				res
+					.status(401)
+					.json({ message: 'The jokes on you, you shall not pass!' });
+			}
+		})
+		.catch(err => {
+			console.log('Error with login', err);
+			res.status(500).json({ error: 'Error with login' });
+		});
 });
 
 module.exports = router;
